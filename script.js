33fafc07f60b97ae23d6208068845a5d67936da5
zaidi <muhammadzaidia.d@gmail.com>
2023-10-28 01:02:54 +0800

    restyle the page

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function validateAge() {
     // Prevent form submission
     return false;
 }
+
+// Remove the initial text from the <h1> element
+document.getElementById("typewriter").textContent = "";
+
+const sentence = "Age Validation Form";
+const title = document.getElementById("typewriter");
+let characterIndex = 0;
+
+function type() {
+    if (characterIndex < sentence.length) {
+        title.textContent += sentence.charAt(characterIndex);
+        characterIndex++;
+        setTimeout(type, 100); // Change 100 to adjust typing speed
+    }
+}
+
+type(); // Start the typewriter effect
